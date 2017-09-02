5e4a26875fb6ae75f10178e07ba7b82e6497c36b
“Cyndi <renatoln@yahoo.com.br>
2017-09-02 10:20:42 -0300

    Trabalhos e Exemplos

---
+++ Exemplos/PilhaPonteiroParImpar.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "minhas_funcoes.h"
+
+int size1 = 5;
+
+void printMenu(){
+	printf("Digite a opção:\n");
+	printf("1 - Inserir\n");
+	printf("2 - Remover elementos\n");
+	printf("3 - Listar\n");
+	
+	printf("0 - Sair\n");
+}
+
+
+int main()
+{
+    printf("Hello Pilha ponteiros!\n");
+    int vetPar[size1];
+	int vetImpar[size1];
+	int qualVetor = 0;
+	int tamPar = 0;
+	int tamImpar = 0;
+	int sair = 0, opcao;
+	while (!sair){
+		printMenu();
+		scanf("%d",&opcao);
+		switch(opcao){
+			case 0:{
+				sair = 1;
+				break;
+			}
+			case 1: {
+				int cheio;
+				cheio = inserirElemento(vetPar, vetImpar, &tamPar, &tamImpar, &qualVetor);
+				if (cheio){
+					
+					if (qualVetor == 2)
+						printf("Vetor PAR Cheio\n");	
+					else
+						printf("Vetor IMPAR Cheio\n");	
+					
+				}else{
+					if (qualVetor == 2)
+						printf("Valor inserido no vetor PAR com sucesso\n");	
+					else
+						printf("Valor inserido no vetor IMPAR com sucesso\n");
+				}
+				break;
+			}
+			case 2: {
+				removerElemento(vetPar, vetImpar, &tamPar, &tamImpar);
+				break;
+			}
+			case 3: {
+				listarElementos(vetPar, vetImpar, tamPar, tamImpar);
+				break;
+			}
+			default{
+				printf("Opção inválida\n");				
+			}
+		}
+	}
+	
+	
+    //printf("Endereco de i: %p\n", p);
+    //printf("Valor de i %d\n", *p);
+    //printf("%p\n", &pinteiro);
+    return 0;
+}
+
+int inserirElemento(int *vetPar, int *vetImpar, int *tamPar, int *tamImpar, int *qualVetor){
+    printf("Digite o elemento\n");
+    int i;
+	int cheio = 0;
+    scanf("%d", &i);
+    if (i % 2 == 0){ //par
+		qualVetor = 2;
+		if (*tamPar < size1){
+			vetPar[*tamPar] = i;
+			//*(vetPar+tamPar) = i;
+			*tamPar = *tamPar + 1; 
+		}else{
+		    cheio = 1;
+		}
+    }else{
+    	qualVetor = 1;
+		if (*tamImpar < size1){
+			vetImpar[*tamImpar] = i;
+			//*(vetPar+tamImpar) = i;
+			*tamImpar = *tamImpar + 1;
+		}else{
+		    cheio = 1;
+		}	
+	
+	}
+	return cheio;
+}
+
+int listarElementos(int *vetPar, int *vetImpar, int tamPar, int tamImpar){
+	printf("Vetor par\n");
+	for (int i = 0; i < tamPar; i++)
+			printf("%d\n",vetPar[i]);
+	printf("Vetor impar\n")	;
+	for (int i = 0; i < tamImpar; i++)
+			printf("%d\n",vetImpar[i]);	
+
+	return 0;	
+}
+
+int removerElemento(int *vetPar, int *vetImpar, int *tamPar, int *tamImpar){
+	int op;
+	printf("Remover último elemento (1) par ou (2) impar?\n");
+	scanf("%d", &op);
+	if (op == 1 && *tamPar > 0) {
+		*tamPar = *tamPar - 1;
+	}else if (op == 2 && *tamImpar > 0){
+		*tamImpar = *tamImpar - 1;
+	}	
+	return 0;	
+}
+
+
---
+++ Trabalho1/aluno1.c
@@ -0,0 +1,150 @@
+// #################################################
+//  Instituto Federal da Bahia
+//  Salvador - BA
+//  Curso de Análise e Desenvolvimento de Sistemas http://ads.ifba.edu.br
+//  Disciplina: INF029 - Laboratório de Programação
+//  Professor: Renato Novais - renato@ifba.edu.br
+
+//  ----- Orientações gerais -----
+//  Descrição: esse arquivo deve conter as questões do trabalho do aluno.
+//  Cada aluno deve renomear esse arquivo para Aluno<MATRICULA>.c
+//  O aluno deve preencher seus dados abaixo, e implementar as questões do trabalho
+
+//  ----- Dados do Aluno -----
+//  Nome:
+//  email:
+//  Matrícula:
+//  Semestre:
+
+//  Copyright © 2016 Renato Novais. All rights reserved.
+// Última atualização: 19/08/2016
+
+// #################################################
+
+#include <stdio.h>
+/*
+ Q1 = validar data
+@objetivo
+    Validar uma data
+@entrada
+    uma string data. Formatos que devem ser aceitos: dd/mm/aaaa, onde dd = dia, mm = mês, e aaaa, igual ao ano. dd em mm podem ter apenas um digito, e aaaa podem ter apenas dois digitos.
+@saida
+    0 -> se data inválida
+    1 -> se data válida
+ */
+int q1(char *data){
+    int datavalida = 1;
+    
+    //printf("%s\n", data);
+    
+    
+    if (datavalida)
+        return 1;
+    else
+        return 0;
+    
+}
+
+/*
+ Q2 = diferença entre duas datas
+ @objetivo
+    Calcular a diferença em anos, meses e dias entre duas datas
+ @entrada
+    uma string datainicial, uma string datafinal. Além disso, a função tem três parâmetros qtdDias, qtdMeses e qtdAnos. Esses três parâmetros devem ser utilizados para guardar os resultados dos cálculos. Na chamada da função deve passar o valor -1 para os três
+ @saida
+    1 -> cálculo de diferença realizado com sucesso
+    2 -> datainicial inválida
+    3 -> datafinal inválida
+    4 -> datainicial < datafinal
+ */
+int q2(char *datainicial, char *datafinal, int *qtdDias, int *qtdMeses, int *qtdAnos){
+    int nDias, nMeses, nAnos;
+
+    nDias = nDias;
+    nMeses = nMeses;
+    nAnos = nAnos;
+    
+
+
+
+
+    *qtdDias = nDias;
+    *qtdAnos = nAnos;
+    *qtdMeses = nMeses;
+    
+    //printf("%s\n", datainicial);
+    //printf("%s\n", datafinal);
+    
+    return 1;
+    
+}
+
+
+/*
+ Q3 = encontrar caracter em texto
+ @objetivo
+    Pesquisar quantas vezes um determinado caracter ocorre em um texto
+ @entrada
+    uma string texto, um caracter c e um inteiro que informa se é uma pesquisa Case Sensitive ou não. Se isCaseSensitive = 1, a pesquisa deve considerar diferências entre maiúsculos e minúsculos.
+        Se isCaseSensitive != 1, a pesquisa não deve  considerar diferências entre maiúsculos e minúsculos.
+ @saida
+    Um número n >= 0. 
+ */
+int q3(char *texto, char c, int isCaseSensitive){
+    int qtdOcorrencias = -1;
+    
+    return qtdOcorrencias;
+    
+}
+
+/*
+ Q4 = encontrar palavra em texto
+ @objetivo
+    Pesquisar todas as ocorrências de uma palavra em um texto
+ @entrada
+    uma string texto base (strTexto), uma string strBusca e um vetor de inteiros (posicoes) que irá guardar as posições de início e fim de cada ocorrência da palavra (strBusca) no texto base (texto).
+ @saida
+    Um número n >= 0 correspondente a quantidade de ocorrências encontradas.
+    O vetor posicoes deve ser preenchido com cada entrada e saída correspondente. Por exemplo, se tiver uma única ocorrência, a posição 0 do vetor deve ser preenchido com o índice de início do texto, e na posição 1, deve ser preenchido com o índice de fim da ocorrencias. Se tiver duas ocorrências, a segunda ocorrência será amazenado nas posições 2 e 3, e assim consecutivamente. Suponha a string "Instituto Federal da Bahia", e palavra de busca "dera". Como há uma ocorrência da palavra de busca no texto, deve-se armazenar no vetor, da seguinte forma:
+        posicoes[0] = 13;
+        posicoes[1] = 16;
+        Observe que o índice da posição no texto deve começar ser contado a partir de 1.
+        O retorno da função, n, nesse caso seria 1;
+
+ */
+int q4(char *strTexto, char *strBusca, int posicoes[30]){
+    int qtdOcorrencias = -1;
+    
+    return qtdOcorrencias;
+    
+}
+
+/*
+ Q5 = inverte número
+ @objetivo
+    Inverter número inteiro
+ @entrada
+    uma int num.
+ @saida
+    Número invertido
+ */
+
+int q5(int num){
+
+    return num;
+}
+
+/*
+ Q5 = ocorrência de um número em outro
+ @objetivo
+    Verificar quantidade de vezes da ocorrência de um número em outro
+ @entrada
+    Um número base (numerobase) e um número de busca (numerobusca).
+ @saida
+    Quantidade de vezes que número de busca ocorre em número base
+ */
+
+int q6(int numerobase, int numerobusca){
+    int qtdOcorrencias;
+    return qtdOcorrencias;
+}
