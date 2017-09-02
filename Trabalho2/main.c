27eec5026345e8eb6e3fde0afd35d994e876e60e
renato novais <renato@ifba.edu.br>
2017-09-02 06:55:52 -0300

    feito estrutura básica do projeto

--- Trabalho2/main.c
+++ Trabalho2/main.c
@@ -1,10 +1,53 @@
 #include <stdio.h> 
 #include <stdlib.h>
 
+int menu();
+
+
+
+int menu(){
+    int op;
+    printf("Digite as opção desejada\n");
+    printf("0 - Sair\n");
+    printf("1 - Inserir\n");
+    printf("2 - Excluir\n");
+    printf("3 - Listar\n");
+    printf("4 - \n");
+    printf("5 - \n");
+    scanf("%d", &op);
+    return op;
+}
+
 int main(){
     
-    printf ("teste\n");
-    printf ("matheus, everton e juliana olhem para ca e continuem olhando\n");
+    int op;
+    int sair = 0;
+    
+    while (!sair){
+        op = menu();
+        switch (op){
+            case 0:{
+                sair =1;
+                break;
+            }
+            case 1:{ //inserir
+                //TODO
+                break;
+            }
+
+            case 2:{ //excluir
+                //TODO
+                break;
+            }
+            default:{
+                printf("opcao inválida\n");
+            }
+
+            
+        }
+        
+        
+    }
     
     return 0;
     
