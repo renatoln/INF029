da27a9cc8db4330c33217f5e9df21d01f9a41621
Renato Novais <renatonovais@gmail.com>
2021-03-24 19:22:09 -0300

    implementado a questão 2

---
+++ listas/Lista1-Funcoes-Respostas/L1-q2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+//assinaturas das funções
+int subtrai(int x, int y, int k);
+
+int main()
+{
+    int a, b, c, resultado;
+    a = 5;
+    b = 6;
+    c = 9;
+
+    resultado = subtrai(a, b, c);
+
+    printf("A subtração  entre %d, %d e %d eh %d\n", a, b, c, resultado);
+}
+
+int subtrai(int x, int y, int k)
+{
+    int res = x - y - k;
+    return res;
+}
