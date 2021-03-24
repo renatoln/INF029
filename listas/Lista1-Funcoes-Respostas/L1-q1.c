aa236001ba9e26311f3f10b634669e8549d42f4c
Renato Novais <renatonovais@gmail.com>
2021-03-24 19:21:50 -0300

    adicionado os parametros na assinatura

--- listas/Lista1-Funcoes-Respostas/L1-q1.c
+++ listas/Lista1-Funcoes-Respostas/L1-q1.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
 //assinaturas das funções
-int soma();
+int soma(int x, int y);
 
-
-int main(){
+int main()
+{
     int a, b, resultado;
     a = 5;
     b = 6;
-    
+
     resultado = soma(a, b);
-    
+
     printf("A soma entre %d e %d eh %d\n", a, b, resultado);
-    
+    return 1;
 }
 
-int soma(int x, int y){
+int soma(int x, int y)
+{
     int res = x + y;
     return res;
 }
