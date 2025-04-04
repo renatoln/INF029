d87bc956479dce268dcec5af99a1908ffbf9fe75
Renato Novais <renatonovais@gmail.com>
2025-04-04 23:42:04 +0000

    adicao da média

--- Exemplos/animais.c
+++ Exemplos/animais.c
@@ -4,7 +4,8 @@
 int main(){ 
     int idade[TAM];
     char sexo[TAM];
-
+    
+    // fazendo a leitura dos dados
     for (int i = 0; i < TAM; i++){
         printf("Digite a idade: ");
         scanf("%d", &idade[i]);
@@ -13,8 +14,23 @@ int main(){
         scanf("%c", &sexo[i]);
     }
 
+    //imprimindo todos
+    printf("#### Todos animais\n");
     for (int i = 0; i < TAM; i++){
         printf("Idade: %d - Sexo: %c\n", idade[i], sexo[i]);
     }
+    
+    // calcular a média
+    int soma = 0;
+    for (int i = 0; i < TAM; i++){
+        soma += idade[i];
+    }
+    int media = soma / TAM;
 
+    //imprimindo maiores que a média
+    printf("#### Animais com idade acima da média\n");
+    for (int i = 0; i < TAM; i++){
+        if (idade[i] > media)
+            printf("Idade: %d - Sexo: %c\n", idade[i], sexo[i]);
+    }
 }
