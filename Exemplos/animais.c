ec7ba687e343d3109b17e39bb9744036476ead11
Renato Novais <renatonovais@gmail.com>
2025-04-05 00:27:39 +0000

    programa com menu e cadastro um a um

--- Exemplos/animais.c
+++ Exemplos/animais.c
@@ -6,6 +6,7 @@ int main(){
     char sexo[TAM];
     int sair = 0;
     int opcao;
+    int qtd = 0;
     
     while (!sair){    //sair == 0    OU    sair != 1
         printf("Opções\n");
@@ -17,12 +18,42 @@ int main(){
 
         switch (opcao){
             case 1:{
+                // fazendo a leitura dos dados
+                if (qtd < TAM){
+                    printf("Digite a idade: ");
+                    scanf("%d", &idade[qtd]);
+                    getchar();
+                    printf("Digite a sexo: ");
+                    scanf("%c", &sexo[qtd]);
+                    qtd ++;
+                }else{
+                    printf("PET cheio");
+                }
+            
                 break;
             }
             case 2:{
+                 //imprimindo todos
+                printf("#### Todos animais\n");
+                for (int i = 0; i < qtd; i++){
+                    printf("Idade: %d - Sexo: %c\n", idade[i], sexo[i]);
+                }
                 break;
             }
             case 3:{
+                // calcular a média
+                int soma = 0;
+                for (int i = 0; i < qtd; i++){
+                    soma += idade[i];
+                }
+                int media = soma / qtd;
+
+                //imprimindo maiores que a média
+                printf("#### Animais com idade acima da média\n");
+                for (int i = 0; i < qtd; i++){
+                    if (idade[i] > media)
+                        printf("Idade: %d - Sexo: %c\n", idade[i], sexo[i]);
+                }
                 break;
             }
             case 0:{
@@ -35,32 +66,9 @@ int main(){
 
     }
     
-    // fazendo a leitura dos dados
-    for (int i = 0; i < TAM; i++){
-        printf("Digite a idade: ");
-        scanf("%d", &idade[i]);
-        getchar();
-        printf("Digite a sexo: ");
-        scanf("%c", &sexo[i]);
-    }
-
-    //imprimindo todos
-    printf("#### Todos animais\n");
-    for (int i = 0; i < TAM; i++){
-        printf("Idade: %d - Sexo: %c\n", idade[i], sexo[i]);
-    }
     
-    // calcular a média
-    int soma = 0;
-    for (int i = 0; i < TAM; i++){
-        soma += idade[i];
-    }
-    int media = soma / TAM;
 
-    //imprimindo maiores que a média
-    printf("#### Animais com idade acima da média\n");
-    for (int i = 0; i < TAM; i++){
-        if (idade[i] > media)
-            printf("Idade: %d - Sexo: %c\n", idade[i], sexo[i]);
-    }
+   
+    
+   
 }
