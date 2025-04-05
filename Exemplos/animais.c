a0f3d6a08783da904d904d6197ea007348066c5c
Renato Novais <renatonovais@gmail.com>
2025-04-05 00:11:42 +0000

    adicao laco de repeticao

--- Exemplos/animais.c
+++ Exemplos/animais.c
@@ -4,6 +4,36 @@
 int main(){ 
     int idade[TAM];
     char sexo[TAM];
+    int sair = 0;
+    int opcao;
+    
+    while (!sair){    //sair == 0    OU    sair != 1
+        printf("Opções\n");
+        printf("1 - Cadastrar\n");
+        printf("2 - Imprimir todos\n");
+        printf("3 - Imprimir maiores que a média\n");
+        printf("0 - Sair\n");
+        scanf("%d", &opcao);
+
+        switch (opcao){
+            case 1:{
+                break;
+            }
+            case 2:{
+                break;
+            }
+            case 3:{
+                break;
+            }
+            case 0:{
+                sair = 1;
+                break;
+            }
+            default: 
+                printf("Opção inválida\n");
+        }
+
+    }
     
     // fazendo a leitura dos dados
     for (int i = 0; i < TAM; i++){
