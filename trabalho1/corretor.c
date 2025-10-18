8dc58c78d9d80860df920d6fc1f4ad8cc8891dcf
Renato Novais <renatonovais@gmail.com>
2025-10-18 00:39:49 +0000

    modificado nome da pasta

--- trabalho1/corretor.c
+++ trabalho1/corretor.c
@@ -35,12 +35,12 @@ int main()
     //testSomar();
     //testFatorial();
     testQ1();
-    testQ2();
+    /*testQ2();
     testQ3();
     testQ4();
     testQ5();
     testQ6();
-    testQ7();
+    testQ7();*/
 }
 
 void testSomar()
