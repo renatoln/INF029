d23d15829ee891b0dcc5eb184c216c0820ba5d75
Renato Novais <renatonovais@gmail.com>
2025-10-31 17:19:46 -0300

    Update novoCorretor.c

--- trabalho1/novoCorretor.c
+++ trabalho1/novoCorretor.c
@@ -2,7 +2,7 @@
 #include<string.h>
 
 //renomeie o arquivo do include abaixo para PrimeiroUltimoNomeMATRICULA.h
-#include "PrimeiroUltimoNomeMATRICULA.h"
+#include "trabalho1.h"
 
 void testQ1();
 void testQ2();
